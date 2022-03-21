999d3b19e5a9673556afee61874f7cf84dd41751
chenrui <646971519@qq.com>
2022-03-21 22:22:24 +0800

    project init

---
+++ src/main/java/ModelTest.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName: ModelTest
+ * @date: 2022/3/21 22:21
+ * @author: chenrui
+ * @Description: 模式代码测试
+ * @Version: 1.0
+ */
+public class ModelTest {
+    public static void main(String[] args) {
+        System.out.printf("模式代码测试");
+    }
+}
